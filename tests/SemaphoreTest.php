7fb330690ded6b321959fabaf2fd180809c98244
Matthew Hotchen <m@mhn.me>
2017-05-09 14:30:07 +0100

    New test for scenario where semaphore was modified
    It could have been modified either by an external call, or by memcache
    being reset

--- tests/SemaphoreTest.php
+++ tests/SemaphoreTest.php
@@ -106,4 +106,28 @@ class SemaphoreTest extends TestCase
         $semaphore->acquire("test");
         $semaphore->release();
     }
+
+    public function testSemaphoreAlteredExternally()
+    {
+        $memcached = Mockery::mock(\Memcached::class);
+        $memcached->shouldIgnoreMissing();
+        $memcached
+            ->shouldReceive('get')
+            ->andReturn(1, null, 1, 0) // the null shouldn't happen unless the semaphore was modified externally or lost
+        ;
+        $memcached
+            ->shouldReceive('getResultCode')
+            ->andReturn(\Memcached::RES_SUCCESS)
+        ;
+        $memcached
+            ->shouldReceive('cas')
+            ->with(null, "semaphore_test", 0) // it decrements the semaphore
+            ->with(null, "semaphore_test", 0) // it decrements the semaphore again (after it was modified and recreated)
+            ->with(null, "semaphore_test", 1) // then returns it to the original value in "release", freeing it
+        ;
+
+        $semaphore = new Semaphore($memcached);
+        $semaphore->acquire("test");
+        $semaphore->release();
+    }
 }
